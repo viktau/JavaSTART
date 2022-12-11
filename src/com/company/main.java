7e7e330f70f003bd15f3b0fc21fc200f8c6a5cac
viktau <tataurov@kpnu.km.ua>
2022-12-11 22:48:11 +0200

    ADD Scanner

--- src/com/company/main.java
+++ src/com/company/main.java
@@ -1,7 +1,11 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class main {
     public static void main (String [] args){
         System.out.println("Hello World");
+        new Number = Scanner(System.io());
+
     }
 }
