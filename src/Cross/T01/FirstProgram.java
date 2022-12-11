1c77f76d50c7d93249bcbaad8d7fae9566ba04fc
viktau <tataurov@kpnu.km.ua>
2022-12-11 23:53:33 +0200

    Create class EnterNumber

--- src/Cross/T01/FirstProgram.java
+++ src/Cross/T01/FirstProgram.java
@@ -1,13 +1,23 @@
 package Cross.T01;
 
+import java.util.Scanner;
+
 public class FirstProgram {
     public static void main (String[] args){
         SloganAction firstObject = new SloganAction();
         firstObject.printSlogan();
+        EnterNumber secondObject = new EnterNumber();
+        secondObject.Number();
     }
 }
 class SloganAction {
     void printSlogan(){
-        System.out.println("Hello ");
+        System.out.println("Enter number ");
+    }
+}
+class EnterNumber {
+    void Number() {
+        Number new Number() = Scanner(System.io);
+
     }
 }
